3d1ac9b30ecfe3cc2fd05afea6f73d93381bdcae
StefanDellamore <dellamoreste.info@gmail.com>
2024-03-14 15:55:23 +0100

    Create route and controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,7 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//Models
+use App\Models\Project;
+
 class ProjectController extends Controller
 {
-    
+    public function index ()
+    {
+        $projects = Project::all();
+
+        return response()->json([
+            'code'=> true,
+            'results'=> $projects,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//controllers
+
+use App\Http\Controllers\Api\ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name ('api.')->group(function(){
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    Route::get('/project', [ProjectController::class, 'index'])->name('api.project.index') ;
 });
